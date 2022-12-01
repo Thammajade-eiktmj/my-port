dd8fa3d7f31b66abe05674d7316358107095cfe6
Thammajade Lertwachara <eik@Thammajades-MacBook-Pro.local>
2022-12-01 20:25:53 +0700

    test deploy1

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  assetPrefix: isProd ? "/my-port/" : "",
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
