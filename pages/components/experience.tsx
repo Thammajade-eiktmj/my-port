263c218e173550404e39ec6ec6b4d389adf08742
Thammajade Lertwachara <eik@Thammajades-MacBook-Pro.local>
2022-12-10 00:25:14 +0700

    fix content

--- pages/components/experience.tsx
+++ pages/components/experience.tsx
@@ -28,48 +28,51 @@ export default function Experience() {
             <div className='mt-10 w-full rounded-[10px] bg-white py-6 px-8 pt-8 shadow'>
               <div className='mt-4 ml-4 flex flex-col items-center justify-center'>
                 <img className='h-10 w-10' src={`${assetPrefix}/ob.png`}></img>
-                <p className='ml-2 mt-4 font-semibold'>Internship at Ookbee U: Career Project</p>
+                <p className='ml-2 mt-4 font-semibold'>Internship at Ookbee U</p>
+                <p className='ml-2 mt-1'>Fullstack Developer, June-December</p>
               </div>
               <p className='m-6 text-center'>
                 {' '}
-                Respectively. When a user locates a position that interests them, they can search
-                and filter the available positions. Despite the fact that the admin page has JWT
-                token authentication and a reset password method that sends an otp to your email
-                when you forget it. The landing page of the website shows the working environment
-                and has a page with filters for the different kinds of jobs that the organization is
-                looking for.{' '}
+                Career Project, Respectively. When a user locates a position that interests them,
+                they can search and filter the available positions. Despite the fact that the admin
+                page has JWT token authentication and a reset password method that sends an otp to
+                your email when you forget it. The landing page of the website shows the working
+                environment and has a page with filters for the different kinds of jobs that the
+                organization is looking for.{' '}
               </p>
             </div>
             <div className='mt-10 w-full rounded-[10px] bg-white py-6 px-8 pt-8 shadow'>
               <div className='mt-4 ml-4 flex flex-col items-center justify-center'>
                 <img className='h-10 w-10' src={`${assetPrefix}/vango.png`}></img>
-                <p className='ml-2 mt-4 font-semibold'>Capstone Project: Reservation Van</p>
+                <p className='ml-2 mt-4 font-semibold'>Capstone Project</p>
+                <p className='ml-2 mt-1'>Fullstack Developer</p>
               </div>
               <p className='m-6 text-center'>
                 {' '}
-                Can allow drivers and passengers to see the schedule time, identify passengers who
-                have registered their cars on their maps, and view the route path, which can
-                estimate time and distance from the driver's location. As a result, we have an admin
-                cms admin who can control the vehicle, schedule, and route.{' '}
+                Reservation Van, Can allow drivers and passengers to see the schedule time, identify
+                passengers who have registered their cars on their maps, and view the route path,
+                which can estimate time and distance from the driver's location. As a result, we
+                have an admin cms admin who can control the vehicle, schedule, and route.{' '}
               </p>
             </div>
             <div className='w-full rounded-[10px] bg-white py-6 px-8 pt-8 shadow'>
               <div className='mt-4 ml-4 flex flex-col items-center justify-center'>
                 <img className='h-10 w-10' src={`${assetPrefix}/plant1.png`}></img>
-                <p className='ml-2 mt-4 font-semibold'>University Project: Reforestation</p>
+                <p className='ml-2 mt-4 font-semibold'>University Project</p>
               </div>
               <p className='m-6 text-center'>
                 {' '}
-                The main idea is to emphasize the value of forests. We created a platform that is
-                based on the SDGs and uses Flutter to code. Users can publish activities related to
-                the forest to invite others to participate in those activities. The system also
-                contains login and profile systems so users can change their own information.{' '}
+                Reforestation, The main idea is to emphasize the value of forests. We created a
+                platform that is based on the SDGs and uses Flutter to code. Users can publish
+                activities related to the forest to invite others to participate in those
+                activities. The system also contains login and profile systems so users can change
+                their own information.{' '}
               </p>
             </div>
             <div className='w-full rounded-[10px] bg-white py-6 px-8 pt-8 shadow'>
               <div className='mt-4 ml-4 flex flex-col items-center justify-center'>
                 <img className='h-10 w-10' src={`${assetPrefix}/programming.png`}></img>
-                <p className='ml-2 mt-4 font-semibold'>Workshop: Javascript</p>
+                <p className='ml-2 mt-4 font-semibold'>Workshop</p>
               </div>
               <div className='m-6'>
                 <p className='text-center'> Postpost(Posting site for random image in post.)</p>
