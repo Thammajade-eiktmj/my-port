f9d366b54f2afc16044b9b53cfbad19dce4be80a
Thammajade-eiktmj <Thammajade-eiktmj@users.noreply.github.com>
2022-12-09 09:58:44 +0000

    deploy: cd767a2d1725f3e646bdb433400075fbae797ac3

---
+++ _next/static/chunks/pages/layout/footer-8960e6b28edc4096.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[863],{4856:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/layout/footer",function(){return u(1162)}])}},function(n){n.O(0,[774,888,179],function(){return n(n.s=4856)}),_N_E=n.O()}]);
---
+++ _next/static/chunks/pages/layout/layout-6bee05e38526101a.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[865],{2558:function(n,u,_){(window.__NEXT_P=window.__NEXT_P||[]).push(["/layout/layout",function(){return _(8073)}])}},function(n){n.O(0,[774,888,179],function(){return n(n.s=2558)}),_N_E=n.O()}]);
