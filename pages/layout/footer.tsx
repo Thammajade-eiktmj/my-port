05181e30a5742d43765bb7d1a7335e33ffca5a48
Thammajade Lertwachara <eik@Thammajades-MacBook-Pro.local>
2022-12-10 15:12:32 +0700

    fix footer

--- pages/layout/footer.tsx
+++ pages/layout/footer.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Footer() {
   return (
     <>
-      <footer className='rounded-lg bg-slate-800 p-4 shadow md:flex md:items-center md:justify-between md:p-6'>
+      <footer className='bg-slate-800 p-4 shadow md:flex md:items-center md:justify-between md:p-6'>
         <span className='text-sm text-slate-400 sm:text-center'>© 2022 . All Rights Reserved.</span>
         <ul className='mt-3 flex flex-wrap items-center text-sm text-slate-400 sm:mt-0'>
           <li>
