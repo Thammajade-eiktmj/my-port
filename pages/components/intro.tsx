846b993852ecab82b7ea76309d556393d9ddb910
Thammajade Lertwachara <eik@Thammajades-MacBook-Pro.local>
2022-12-11 14:46:33 +0700

    x

--- pages/components/intro.tsx
+++ pages/components/intro.tsx
@@ -9,8 +9,13 @@ export default function Intro() {
         <div className='px-[15rem]'>
           <div className='container mx-auto flex flex-col'>
             <div className='flex h-screen items-center justify-around'>
-              <div className=''>
-                <img src={`${assetPrefix}/iphone.png`} />
+              <div className='box'>
+                <div
+                  className='inner'
+                  style={{
+                    backgroundImage: `url(${assetPrefix}/bgip.webp)`,
+                  }}
+                ></div>
               </div>
               <div className='justify-self-end rounded-[10px] p-10'>
                 <div className='flex flex-col items-center justify-center justify-self-end'>
@@ -159,7 +164,7 @@ export default function Intro() {
                       window.open('eik.pdf')
                     }}
                     type='button'
-                    className='z-10 group relative mt-6 mb-2 mr-2 inline-flex items-center justify-center overflow-hidden rounded-lg bg-gradient-to-r from-sky-400 to-emerald-600 p-0.5 text-sm font-medium text-slate-50 shadow-2xl shadow-slate-900 hover:text-white focus:outline-none focus:ring-4 focus:ring-green-800 group-hover:from-green-400 group-hover:to-blue-600'
+                    className='group relative z-10 mt-6 mb-2 mr-2 inline-flex items-center justify-center overflow-hidden rounded-lg bg-gradient-to-r from-sky-400 to-emerald-600 p-0.5 text-sm font-medium text-slate-50 shadow-2xl shadow-slate-900 hover:text-white focus:outline-none focus:ring-4 focus:ring-green-800 group-hover:from-green-400 group-hover:to-blue-600'
                   >
                     <span className='relative rounded-md bg-slate-900 px-5 py-2.5 transition-all duration-75 ease-in group-hover:bg-opacity-0'>
                       Download CV
