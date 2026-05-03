6e6198cfd7d854aa40f2f594068e5a7b2dbbf7cf
Thammajade-eiktmj <Thammajade-eiktmj@users.noreply.github.com>
2026-05-03 20:16:58 +0000

    deploy: 747f983af1394df10ab754ee213c2691d2b36608

--- _next/static/7BvdHEhNkldngqm1r_2LG/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-bc6ba40666e3df8d.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
